0aa2fa82d526eb6d0704e541bfb506d2d4ce821e
Sands <82897602+Sands-45@users.noreply.github.com>
2021-06-22 04:36:27 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,25 +1,32 @@
 function openTab(evt, tabName) {
-	var i, x, tablinks;
-	x = document.getElementsByClassName("content-tab");
-	for (i = 0; i < x.length; i++) {
-		x[i].style.display = "none";
-	}
-	tablinks = document.getElementsByClassName("tab");
-	for (i = 0; i < x.length; i++) {
-		tablinks[i].className = tablinks[i].className.replace(" is-active", "");
-	}
-	document.getElementById(tabName).style.display = "block";
-	evt.currentTarget.className += " is-active";
+  var i, x, tablinks;
+  x = document.getElementsByClassName("content-tab");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
   }
-  //----------------Projects
+  tablinks = document.getElementsByClassName("tab");
+  for (i = 0; i < x.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" is-active", "");
+  }
+  document.getElementById(tabName).style.display = "block";
+  evt.currentTarget.className += " is-active";
+}
+//Back To Top
+//Get the button:
+mybutton = document.getElementById("top");
 
- $(".modal-button").click(function() {
-	var target = $(this).data("target");
-	$("html").addClass("is-clipped");
-	$(target).addClass("is-active");
- });
- 
- $(".modal-close").click(function() {
-	$("html").removeClass("is-clipped");
-	$(this).parent().removeClass("is-active");
- });
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (
+    document.body.scrollTop > 600 ||
+    document.documentElement.scrollTop > 600
+  ) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
