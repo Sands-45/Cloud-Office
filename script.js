28e2a43e5e6e2443c5bc6e132a8e946ee9c08c57
Sands <82897602+Sands-45@users.noreply.github.com>
2021-06-30 11:02:37 +0200

    Update script.js

--- script.js
+++ script.js
@@ -11,6 +11,20 @@ function openTab(evt, tabName) {
   document.getElementById(tabName).style.display = "block";
   evt.currentTarget.className += " is-active";
 }
+// Dashboards-----------------------
+function openDash(clk, dashName) {
+  var d, e, dashlinks;
+  e = document.getElementsByClassName("dash-content");
+  for (d = 0; d < e.length; d++) {
+    e[d].style.display = "none";
+  }
+  dashlinks = document.getElementsByClassName("board");
+  for (d = 0; d < e.length; d++) {
+    dashlinks[d].className = dashlinks[d].className.replace("is-active", "");
+  }
+  document.getElementById(dashName).style.display = "block";
+  clk.currentTarget.className += " is-active";
+}
 //Back To Top
 //Get the button:
 mybutton = document.getElementById("top");
@@ -31,34 +45,32 @@ function scrollFunction() {
   }
 }
 /*---------------------------------------------------*/
-document.addEventListener('DOMContentLoaded', function () {
-
+//Mobile Nav burger
+document.addEventListener("DOMContentLoaded", function () {
   // Get all "navbar-burger" elements
-  var $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
+  var $navbarBurgers = Array.prototype.slice.call(
+    document.querySelectorAll(".navbar-burger"),
+    0
+  );
 
   // Check if there are any nav burgers
   if ($navbarBurgers.length > 0) {
-
     // Add a click event on each of them
     $navbarBurgers.forEach(function ($el) {
-      $el.addEventListener('click', function () {
-
+      $el.addEventListener("click", function () {
         // Get the target from the "data-target" attribute
         var target = $el.dataset.target;
         var $target = document.getElementById(target);
 
         // Toggle the class on both the "navbar-burger" and the "navbar-menu"
-        $el.classList.toggle('is-active');
-        $target.classList.toggle('is-active');
-
+        $el.classList.toggle("is-active");
+        $target.classList.toggle("is-active");
       });
     });
   }
-
 });
 //Tools access
 function ValidatePassword() {
-
   var password = prompt("Please enter password to proceed", "");
 
   if (password == "assurance@123") {
@@ -68,7 +80,6 @@ function ValidatePassword() {
 }
 //cloud Sign-In
 function ValidatePassword1() {
-
   var password1 = prompt("Please enter password to proceed", "");
 
   if (password1 == "dialndine@123") {
